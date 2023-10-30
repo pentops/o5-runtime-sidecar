fc4a3e7586738177a4d053b4e7e7db9b33ebfd0a
Damien Whitten <dw@chaos404.com>
2023-10-29 20:14:27 -0700

    Outbox

---
+++ outbox/sns.go
@@ -0,0 +1,82 @@
+package outbox
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+	"github.com/aws/aws-sdk-go-v2/service/sns/types"
+)
+
+type SNSAPI interface {
+	PublishBatch(ctx context.Context, params *sns.PublishBatchInput, optFns ...func(*sns.Options)) (*sns.PublishBatchOutput, error)
+}
+
+type SNSBatcher struct {
+	client SNSAPI
+	prefix string
+}
+
+func NewSNSBatcher(client SNSAPI, prefix string) *SNSBatcher {
+	return &SNSBatcher{
+		client: client,
+		prefix: prefix,
+	}
+}
+
+func (b *SNSBatcher) SendBatch(ctx context.Context, destination string, msgs []*Message) error {
+
+	dest := b.prefix + destination
+
+	for len(msgs) > 0 {
+		var batch []*Message
+		if len(msgs) > 10 {
+			batch = msgs[:10]
+			msgs = msgs[10:]
+		} else {
+			batch = msgs
+			msgs = nil
+		}
+
+		var entries []types.PublishBatchRequestEntry
+		for _, msg := range batch {
+			body := base64.StdEncoding.EncodeToString(msg.Message)
+			attributes := map[string]types.MessageAttributeValue{}
+			for key, vals := range msg.Headers {
+				for _, val := range vals {
+					if val == "" {
+						continue
+					}
+					attributes[key] = types.MessageAttributeValue{
+						StringValue: aws.String(val),
+						DataType:    aws.String("String"),
+					}
+				}
+			}
+			entries = append(entries, types.PublishBatchRequestEntry{
+				Id:                aws.String(msg.ID),
+				Message:           aws.String(body),
+				MessageAttributes: attributes,
+			})
+		}
+
+		out, err := b.client.PublishBatch(ctx, &sns.PublishBatchInput{
+			PublishBatchRequestEntries: entries,
+			TopicArn:                   aws.String(dest),
+		})
+		if err != nil {
+			return err
+		}
+		if len(out.Failed) > 0 {
+			// TODO: Check if this can come up for some but not all. In theory,
+			// only permissions and size should throw, we can pre-check size and permissions
+			// should be the same for all.
+			ff := out.Failed[0]
+			return fmt.Errorf("at least one message failed: %s: %s", *ff.Code, *ff.Message)
+		}
+	}
+
+	return nil
+}
---
+++ outbox/sns_test.go
@@ -0,0 +1,51 @@
+package outbox
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+)
+
+type mockSNSAPI struct {
+	requests []*sns.PublishBatchInput
+}
+
+func (m *mockSNSAPI) PublishBatch(ctx context.Context, params *sns.PublishBatchInput, optFns ...func(*sns.Options)) (*sns.PublishBatchOutput, error) {
+	m.requests = append(m.requests, params)
+	return &sns.PublishBatchOutput{}, nil
+}
+
+func TestSNSBatcher(t *testing.T) {
+	mock := &mockSNSAPI{}
+	sb := NewSNSBatcher(mock, "prefix-")
+
+	messages := make([]*Message, 21)
+	for idx := range messages {
+		messages[idx] = &Message{
+			ID:      fmt.Sprintf("id%d", idx),
+			Message: []byte(fmt.Sprintf("message %d", idx)),
+			Headers: map[string][]string{
+				"foo": {fmt.Sprintf("foo%d", idx)},
+			},
+		}
+	}
+
+	if err := sb.SendBatch(context.Background(), "foo", messages); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(mock.requests) != 3 {
+		t.Fatalf("expected 3 requests, got %d", len(mock.requests))
+	}
+	if len(mock.requests[0].PublishBatchRequestEntries) != 10 {
+		t.Fatalf("expected 10 entries, got %d", len(mock.requests[0].PublishBatchRequestEntries))
+	}
+	if len(mock.requests[1].PublishBatchRequestEntries) != 10 {
+		t.Fatalf("expected 10 entries, got %d", len(mock.requests[1].PublishBatchRequestEntries))
+	}
+	if len(mock.requests[2].PublishBatchRequestEntries) != 1 {
+		t.Fatalf("expected 1 entries, got %d", len(mock.requests[2].PublishBatchRequestEntries))
+	}
+}
