2a2106d90975380a101e561f0e427683c83e2c4c
Damien Whitten <dw@chaos404.com>
2023-12-01 12:58:58 +1100

    Move main stuff to a library

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,21 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"os"
 	"strings"
 
-	"golang.org/x/sync/errgroup"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/protobuf/reflect/protoreflect"
-
 	"github.com/pentops/custom-proto-api/jsonapi"
-	"github.com/pentops/custom-proto-api/swagger"
-	"github.com/pentops/o5-runtime-sidecar/jwtauth"
 	"github.com/pentops/o5-runtime-sidecar/outbox"
-	"github.com/pentops/o5-runtime-sidecar/protoread"
-	"github.com/pentops/o5-runtime-sidecar/proxy"
+	"github.com/pentops/o5-runtime-sidecar/runtime"
 	"github.com/pentops/o5-runtime-sidecar/sqslink"
 	"gopkg.daemonl.com/envconf"
 
@@ -25,7 +16,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sns"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 
-	"github.com/pentops/jwtauth/jwks"
 	"github.com/pentops/log.go/log"
 )
 
@@ -79,53 +69,37 @@ func run(ctx context.Context, envConfig EnvConfig) error {
 		WrapOneof: true,
 	}
 
-	var snsSender *outbox.SNSBatcher
+	rt := runtime.Runtime{}
 
 	if envConfig.PostgresOutboxURI != "" || envConfig.SQSURL != "" {
 		if envConfig.SNSPrefix == "" {
 			return fmt.Errorf("SNS prefix required when using Postgres outbox")
 		}
-		snsSender = outbox.NewSNSBatcher(sns.NewFromConfig(awsConfig), envConfig.SNSPrefix)
+		rt.Sender = outbox.NewSNSBatcher(sns.NewFromConfig(awsConfig), envConfig.SNSPrefix)
 	}
 
-	var worker *sqslink.Worker
 	if envConfig.SQSURL != "" {
 		sqsClient := sqs.NewFromConfig(awsConfig)
-		worker = sqslink.NewWorker(sqsClient, envConfig.SQSURL, snsSender)
+		rt.Worker = sqslink.NewWorker(sqsClient, envConfig.SQSURL, rt.Sender)
 	}
 
-	var router *proxy.Router
 	if envConfig.PublicPort != 0 {
-		router = proxy.NewRouter(codecOptions)
+		if err := rt.AddRouter(envConfig.PublicPort, codecOptions); err != nil {
+			return fmt.Errorf("add router: %w", err)
+		}
 	}
 
 	if envConfig.StaticFiles != "" {
-		if router == nil {
-			return fmt.Errorf("static files configured but no public port")
+		if err := rt.StaticFiles(envConfig.StaticFiles); err != nil {
+			return fmt.Errorf("static files: %w", err)
 		}
-
-		router.SetNotFoundHandler(http.FileServer(http.Dir(envConfig.StaticFiles)))
 	}
 
-	eg, ctx := errgroup.WithContext(ctx)
-
-	var jwksManager *jwks.JWKSManager
-	if router != nil && len(envConfig.JWKS) > 0 {
-
-		jwksManager = jwks.NewKeyManager()
-
-		if err := jwksManager.AddSourceURLs(envConfig.JWKS...); err != nil {
-			return err
+	if len(envConfig.JWKS) > 0 {
+		if err := rt.AddJWKS(ctx, envConfig.JWKS...); err != nil {
+			return fmt.Errorf("add JWKS: %w", err)
 		}
-
-		router.AuthFunc = jwtauth.JWKSAuthFunc(jwksManager)
-		eg.Go(func() error {
-			return jwksManager.Run(ctx)
-		})
-
 	}
-
-	httpServices := make([]protoreflect.ServiceDescriptor, 0)
 
 	endpoints := strings.Split(envConfig.Service, ",")
 	for _, endpoint := range endpoints {
@@ -133,96 +107,11 @@ func run(ctx context.Context, envConfig EnvConfig) error {
 		if endpoint == "" {
 			continue
 		}
-
-		conn, err := grpc.DialContext(ctx, envConfig.Service, grpc.WithTransportCredentials(insecure.NewCredentials()))
-		if err != nil {
-			return fmt.Errorf("dial: %w", err)
-		}
-		defer conn.Close()
-
-		services, err := protoread.FetchServices(ctx, conn)
-		if err != nil {
-			return fmt.Errorf("fetch: %w", err)
-		}
-
-		// TODO: CORS
-		// TODO: Logging
-		// TODO: Metrics
-		// TODO: Custom forwarding headers
-
-		for _, ss := range services {
-			name := string(ss.FullName())
-			switch {
-			case strings.HasSuffix(name, "Service"):
-				if router == nil {
-					return fmt.Errorf("service %s requires a public port", name)
-				}
-				if err := router.RegisterService(ss, conn); err != nil {
-					return fmt.Errorf("register service %s: %w", name, err)
-				}
-				httpServices = append(httpServices, ss)
-			case strings.HasSuffix(name, "Topic"):
-				if worker == nil {
-					return fmt.Errorf("topic %s requires an SQS URL", name)
-				}
-				if err := worker.RegisterService(ss, conn); err != nil {
-					return fmt.Errorf("register worker %s: %w", name, err)
-				}
-			default:
-				log.WithField(ctx, "service", name).Error("Unknown service type")
-				// but continue
-			}
+		if err := rt.AddEndpoint(ctx, endpoint); err != nil {
+			return fmt.Errorf("add endpoint %s: %w", endpoint, err)
 		}
 	}
 
-	if router == nil && worker == nil && envConfig.PostgresOutboxURI == "" {
-		return fmt.Errorf("no router and no worker. Nothing to do")
-	}
-
-	if router != nil {
-		swaggerDocument, err := swagger.Build(router.CodecOptions, httpServices)
-		if err != nil {
-			return fmt.Errorf("building swagger: %w", err)
-		}
-
-		if err := router.StaticJSON("/swagger.json", swaggerDocument); err != nil {
-			return err
-		}
-
-		srv := http.Server{
-			Handler: router,
-			Addr:    fmt.Sprintf(":%d", envConfig.PublicPort),
-		}
-
-		if jwksManager != nil {
-			// Wait for keys to be loaded before starting the server
-			if err := jwksManager.WaitForKeys(ctx); err != nil {
-				return fmt.Errorf("failed to load JWKS: %w", err)
-			}
-		}
-
-		go func() {
-			<-ctx.Done()
-			srv.Shutdown(ctx) // nolint:errcheck
-		}()
-
-		eg.Go(func() error {
-			return srv.ListenAndServe()
-		})
-	}
-
-	if worker != nil {
-		eg.Go(func() error {
-			return worker.Run(ctx)
-		})
-	}
-
-	if envConfig.PostgresOutboxURI != "" {
-		eg.Go(func() error {
-			return outbox.Listen(ctx, envConfig.PostgresOutboxURI, snsSender)
-		})
-	}
-
-	return eg.Wait()
+	return rt.Run(ctx)
 
 }
---
+++ runtime/runtime.go
@@ -0,0 +1,190 @@
+package runtime
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/pentops/custom-proto-api/jsonapi"
+	"github.com/pentops/custom-proto-api/swagger"
+	"github.com/pentops/jwtauth/jwks"
+	"github.com/pentops/log.go/log"
+	"github.com/pentops/o5-runtime-sidecar/jwtauth"
+	"github.com/pentops/o5-runtime-sidecar/outbox"
+	"github.com/pentops/o5-runtime-sidecar/protoread"
+	"github.com/pentops/o5-runtime-sidecar/proxy"
+	"github.com/pentops/o5-runtime-sidecar/sqslink"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+type Runtime struct {
+	router     *proxy.Router
+	Worker     *sqslink.Worker
+	Sender     *outbox.SNSBatcher
+	JWKS       *jwks.JWKSManager
+	PublicPort int
+
+	httpServices []protoreflect.ServiceDescriptor
+
+	outboxURIs []string
+}
+
+func NewRuntime() *Runtime {
+	return &Runtime{
+		httpServices: make([]protoreflect.ServiceDescriptor, 0),
+	}
+}
+
+func (rt *Runtime) Run(ctx context.Context) error {
+	eg, ctx := errgroup.WithContext(ctx)
+
+	didAnything := false
+
+	if rt.JWKS != nil {
+		// JWKS doesn't count as doint something without a router
+		eg.Go(func() error {
+			return rt.JWKS.Run(ctx)
+		})
+	}
+
+	for _, uri := range rt.outboxURIs {
+		didAnything = true
+		uri := uri
+		eg.Go(func() error {
+			return outbox.Listen(ctx, uri, rt.Sender)
+		})
+	}
+
+	if rt.router != nil {
+		didAnything = true
+		swaggerDocument, err := swagger.Build(rt.router.CodecOptions, rt.httpServices)
+		if err != nil {
+			return fmt.Errorf("building swagger: %w", err)
+		}
+
+		if err := rt.router.StaticJSON("/swagger.json", swaggerDocument); err != nil {
+			return err
+		}
+
+		srv := http.Server{
+			Handler: rt.router,
+			Addr:    fmt.Sprintf(":%d", rt.PublicPort),
+		}
+
+		if rt.JWKS != nil {
+			// Wait for keys to be loaded before starting the server
+			if err := rt.JWKS.WaitForKeys(ctx); err != nil {
+				return fmt.Errorf("failed to load JWKS: %w", err)
+			}
+		}
+
+		go func() {
+			<-ctx.Done()
+			srv.Shutdown(ctx) // nolint:errcheck
+		}()
+
+		eg.Go(func() error {
+			return srv.ListenAndServe()
+		})
+	}
+
+	if rt.Worker != nil {
+		didAnything = true
+		eg.Go(func() error {
+			return rt.Worker.Run(ctx)
+		})
+	}
+
+	if !didAnything {
+		return fmt.Errorf("no services configured")
+	}
+
+	return eg.Wait()
+}
+
+func (rt *Runtime) AddRouter(port int, codecOptions jsonapi.Options) error {
+	if rt.router != nil {
+		return fmt.Errorf("router already configured")
+	}
+
+	rt.router = proxy.NewRouter(codecOptions)
+	rt.PublicPort = port
+	return nil
+}
+func (rt *Runtime) AddOutbox(ctx context.Context, outboxURI string) error {
+	if rt.Sender != nil {
+		return fmt.Errorf("outbox requires a sender")
+	}
+
+	rt.outboxURIs = append(rt.outboxURIs, outboxURI)
+	return nil
+}
+
+func (rt *Runtime) StaticFiles(dirname string) error {
+	if rt.router == nil {
+		return fmt.Errorf("static files configured but no router")
+	}
+
+	rt.router.SetNotFoundHandler(http.FileServer(http.Dir(dirname)))
+	return nil
+}
+
+func (rt *Runtime) AddJWKS(ctx context.Context, sources ...string) error {
+	jwksManager := jwks.NewKeyManager()
+
+	if err := jwksManager.AddSourceURLs(sources...); err != nil {
+		return err
+	}
+
+	rt.router.AuthFunc = jwtauth.JWKSAuthFunc(jwksManager)
+	return nil
+}
+
+func (rt *Runtime) AddEndpoint(ctx context.Context, endpoint string) error {
+
+	conn, err := grpc.DialContext(ctx, endpoint, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return fmt.Errorf("dial: %w", err)
+	}
+	defer conn.Close()
+
+	services, err := protoread.FetchServices(ctx, conn)
+	if err != nil {
+		return fmt.Errorf("fetch: %w", err)
+	}
+
+	// TODO: CORS
+	// TODO: Logging
+	// TODO: Metrics
+	// TODO: Custom forwarding headers
+
+	for _, ss := range services {
+		name := string(ss.FullName())
+		switch {
+		case strings.HasSuffix(name, "Service"):
+			if rt.router == nil {
+				return fmt.Errorf("service %s requires a public port", name)
+			}
+			if err := rt.router.RegisterService(ss, conn); err != nil {
+				return fmt.Errorf("register service %s: %w", name, err)
+			}
+			rt.httpServices = append(rt.httpServices, ss)
+		case strings.HasSuffix(name, "Topic"):
+			if rt.Worker == nil {
+				return fmt.Errorf("topic %s requires an SQS URL", name)
+			}
+			if err := rt.Worker.RegisterService(ss, conn); err != nil {
+				return fmt.Errorf("register worker %s: %w", name, err)
+			}
+		default:
+			log.WithField(ctx, "service", name).Error("Unknown service type")
+			// but continue
+		}
+	}
+
+	return nil
+}
