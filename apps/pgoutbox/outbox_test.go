78913985974e67c8107a4e82b19edf541023d225
Daniel Hess <dan9186@gmail.com>
2025-04-09 13:11:49 -0700

    cleaning up tests and make them stop hanging indefinitely

--- apps/pgoutbox/outbox_test.go
+++ apps/pgoutbox/outbox_test.go
@@ -3,6 +3,7 @@ package pgoutbox
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log/slog"
 	"net/url"
 	"os"
@@ -48,11 +49,9 @@ func (tc testConnector) Name() string {
 	return "test"
 }
 
-var _ pgConnector = testConnector{}
-
 const dbName = "test_outbox_24564546765"
 
-func emptyDB(ctx context.Context, t *testing.T, name string) *pgx.ConnConfig {
+func getNewDB(ctx context.Context, t *testing.T, name string) *pgx.Conn {
 	dbURL := os.Getenv("TEST_DB")
 	if !strings.Contains(dbURL, "test") {
 		t.Fatal("TEST_DB not a test database, it must contain the word 'test' somewhere in the connection string")
@@ -74,12 +73,16 @@ func emptyDB(ctx context.Context, t *testing.T, name string) *pgx.ConnConfig {
 	}
 	defer conn.Close(ctx)
 
-	_, err = conn.Exec(ctx, "DROP DATABASE "+name)
+	q := fmt.Sprintf("DROP DATABASE IF EXISTS %s", name)
+
+	_, err = conn.Exec(ctx, q)
 	if err != nil {
 		t.Logf("failed to drop test database: %s", err)
 	}
 
-	_, err = conn.Exec(ctx, "CREATE DATABASE "+name)
+	q = fmt.Sprintf("CREATE DATABASE %s", name)
+
+	_, err = conn.Exec(ctx, q)
 	if err != nil {
 		t.Fatalf("failed to create test database: %s", err)
 	}
@@ -92,23 +95,7 @@ func emptyDB(ctx context.Context, t *testing.T, name string) *pgx.ConnConfig {
 		t.Fatalf("failed to parse new db url: %s", err)
 	}
 
-	return newConf
-}
-
-func TestOutbox(t *testing.T) {
-	log.DefaultLogger.SetLevel(slog.LevelDebug)
-
-	ctx := context.Background()
-
-	dbconf := emptyDB(ctx, t, dbName)
-
-	dbConn, err := pgx.ConnectConfig(ctx, dbconf)
-	if err != nil {
-		t.Fatalf("failed to connect to db: %s", err)
-	}
-	defer dbConn.Close(ctx)
-
-	sql, err := goose.OpenDBWithDriver("pgx", dbconf.ConnString())
+	sql, err := goose.OpenDBWithDriver("pgx", newConf.ConnString())
 	if err != nil {
 		t.Fatalf("failed to open db: %s", err)
 	}
@@ -117,16 +104,31 @@ func TestOutbox(t *testing.T) {
 		t.Fatal(err.Error())
 	}
 
-	source := sidecar.AppInfo{}
+	newConn, err := pgx.ConnectConfig(ctx, newConf)
+	if err != nil {
+		t.Fatalf("failed to connect to db: %s", err)
+	}
+
+	return newConn
+}
+
+func TestOutbox(t *testing.T) {
+	log.DefaultLogger.SetLevel(slog.LevelDebug)
+
+	ctx := context.Background()
+
+	db := getNewDB(ctx, t, dbName)
+	defer db.Close(ctx)
+
 	batcher := &testBatcher{
 		chMsg: make(chan []*messaging_pb.Message),
 	}
 
 	conn := testConnector{
-		dsn: dbconf.ConnString(),
+		dsn: db.Config().ConnString(),
 	}
 
-	conv := msgconvert.NewConverter(source)
+	conv := msgconvert.NewConverter(sidecar.AppInfo{})
 	o, err := NewOutbox(conn, batcher, conv)
 	if err != nil {
 		t.Fatalf("failed to create outbox listener: %s", err)
@@ -138,26 +140,24 @@ func TestOutbox(t *testing.T) {
 	defer cancel()
 
 	go func() {
-		err := o.Run(outboxCtx)
-		t.Logf("outbox exited: %s", err)
-		runErr <- err
+		runErr <- o.Run(outboxCtx)
 	}()
 
 	sendReceive := func(ids ...string) {
-		_, err = dbConn.Exec(ctx, "BEGIN")
+		_, err = db.Exec(ctx, "BEGIN")
 		if err != nil {
 			t.Fatalf("failed to begin transaction: %s", err)
 		}
 
 		for _, id := range ids {
 			// send a message
-			_, err = dbConn.Exec(ctx, "INSERT INTO outbox (id, data, headers) VALUES ($1,$2,$3);", id, "{}", "")
+			_, err = db.Exec(ctx, "INSERT INTO outbox (id, data, headers) VALUES ($1,$2,$3);", id, "{}", "")
 			if err != nil {
 				t.Fatalf("failed to insert message: %s", err)
 			}
 		}
 
-		_, err = dbConn.Exec(ctx, "COMMIT")
+		_, err = db.Exec(ctx, "COMMIT")
 		if err != nil {
 			t.Fatalf("failed to commit transaction: %s", err)
 		}
@@ -167,18 +167,23 @@ func TestOutbox(t *testing.T) {
 			want[id] = true
 		}
 
-		batch := <-batcher.chMsg
-		for _, msg := range batch {
-			_, ok := want[msg.MessageId]
-			if !ok {
-				t.Errorf("unexpected message: %s", msg.MessageId)
+		select {
+		case batch := <-batcher.chMsg:
+			for _, msg := range batch {
+				_, ok := want[msg.MessageId]
+				if !ok {
+					t.Errorf("unexpected message: %s", msg.MessageId)
+				}
+
+				delete(want, msg.MessageId)
 			}
 
-			delete(want, msg.MessageId)
-		}
+			if len(want) > 0 {
+				t.Errorf("missing messages: %v", want)
+			}
 
-		if len(want) > 0 {
-			t.Errorf("missing messages: %v", want)
+		case <-time.After(time.Second * 5):
+			t.Errorf("timed out waiting for messages")
 		}
 	}
 
@@ -189,7 +194,7 @@ func TestOutbox(t *testing.T) {
 	sendReceive(uuid.NewString(), uuid.NewString())
 
 	// boot the listener
-	_, err = dbConn.Exec(ctx, `
+	_, err = db.Exec(ctx, `
 			SELECT pg_terminate_backend(pg_stat_activity.pid)
 			FROM pg_stat_activity
 			WHERE pg_stat_activity.datname = $1
